480a0706f75e8055858f3a3f1b1329609f9c381c
dbusegit <35306506+bdavor@users.noreply.github.com>
2022-10-16 19:26:19 +0200

    Provjera unesenih podataka
    Provjera korisničkog imena i lozinke na strani Baze podataka i obrada povratne informacije.

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -13,3 +13,34 @@ function validateLoginForm(){
 	}
 	return result;
 }
+
+function sendLoginData(){
+	
+	let xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (4 == this.readyState && 200 == this.status) { 
+			var responseJson = JSON.parse(xhttp.responseText);
+			  if(responseJson.isLoggedIn){
+				  successfulLogin(responseJson.data);
+			  }else{
+				  unsuccessfulLogin(responseJson.reason);
+			  }
+		}
+	 }
+	xhttp.open("GET", "GlavniServlet?action=prijava"  +
+			"&username=" + document.forms["login-form"]["username"].value + 
+			"&password=" + document.forms["login-form"]["password"].value, true);
+	xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
+	xhttp.send();
+	
+}
+
+function successfulLogin(dataJson){
+	window.location.replace(dataJson.redirectUrl);
+}
+
+function unsuccessfulLogin(reason){
+	var authWarning = document.getElementById('auth-warning');
+	authWarning.innerHTML = "Nalog s datim kredencijalima ne postoji.";
+	authWarning.removeAttribute('hidden');
+}
